2efc7093cac467d787053278851dd094e46c9cd4
168206233 <43798276+TsinghuaDream@users.noreply.github.com>
2019-01-01 17:45:11 +0800

    Update TZS.py

--- 168206233/TZS.py
+++ 168206233/TZS.py
@@ -1,14 +1,4 @@
-suspects = ['A', 'B', 'C', 'D']
-criminal_dict = {0: 'innocent', 1: 'guilty'}
-n = 1
-
-for a in range(0, 2):
-    for b in range(0,2):
-        for c in range(0,2):
-            for d in range(0,2):
-                  if (a + b >= 1) and (a + e + f >= 2) and (a * d == 0) and (b == c) and (c + d == 1) and (d >= e): 
-                            result = zip(suspects, [criminal_dict[a], criminal_dict[b], criminal_dict[c], criminal_dict[d])
-                            print "第%d种作案方案:" %(n)
-                            print result
-                            n = n + 1
-print "\n总共有%d种作案方案" % (n-1)
+l=['A','B','C','D']
+for i in l:
+    if (i!='A')+(i=='D')+(i=='B')+(i!='D')==1:
+        print("偷走钻石的犯罪嫌疑人是："+i)
